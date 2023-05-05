08b3fc6aa8834ad63bc5d35b4b73035a9c8fa368
Antonio <antonio@openroad.es>
2023-05-05 17:16:50 +0200

    Fix compose preview broken because BondedBlePeripherals had a reference to BleManager. Created a Fake version for previews

---
+++ src/main/java/io/openroad/filetransfer/ble/peripheral/BondedBlePeripheralsFake.kt
@@ -0,0 +1,25 @@
+package io.openroad.filetransfer.ble.peripheral
+
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+/**
+ * Created by Antonio García (antonio@openroad.es)
+ */
+
+class BondedBlePeripheralsFake() : BondedBlePeripherals {
+    private val _peripheralsData = MutableStateFlow(getPeripheralsData())
+    override val peripheralsData = _peripheralsData.asStateFlow()
+
+    private fun getPeripheralsData(): List<BondedBlePeripherals.Data> {
+        return emptyList()
+    }
+
+    override fun add(name: String?, address: String) {
+
+    }
+
+    override fun remove(address: String) {
+
+    }
+}
